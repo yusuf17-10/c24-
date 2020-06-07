8099df7e255c5144cfab6d425937df2501c11678
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2020-06-07 12:30:27 +0530

    Add files via upload

---
+++ ShootBall.js
@@ -0,0 +1,24 @@
+class ShootBall{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+
+    }
+
+    attach(body){
+        
+    }
+
+    shoot(){
+
+    }
+
+    display(){
+
+    }
+
+}
